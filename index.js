86d4d2ef19e404ca4cb2e547ca773049f0a590ea
DevLucasGodoy <54687280+DevLucasGodoy@users.noreply.github.com>
2022-11-16 21:47:37 -0400

    Revert "código para criptação"
    This reverts commit 77decdd5f334ce2c4a299fcaf64efec73110dafc.

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ require("dotenv-safe").config();
 const jwt = require('jsonwebtoken');
 var { expressjwt: expressJWT } = require("express-jwt");
 const cors = require('cors');
-const crypt = require('./cryptografia')
 
 var cookieParser = require('cookie-parser')
 
